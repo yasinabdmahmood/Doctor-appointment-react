3c07f54c72fb64bc952c3ab054c8b75dabd2cba0
Kamba56 <tomajohn316@yahoo.com>
2023-01-16 21:44:34 +0100

    Add reducers for appointment and doctors

--- src/redux/appointments/appointment.js
+++ src/redux/appointments/appointment.js
@@ -1,5 +1,54 @@
-const appointmentReducer = (state = [], action) => {
-  return state;
+const ADD_APP = 'doctorApp/appointments/ADD_APPOINTMENT';
+const FETCH_APP = 'doctorApp/appointments/FETCH_APPOINTMENT';
+
+const appointmentReducer = (state = intial, action) => {
+  switch (action.type) {
+    case FETCH_APP:
+      return [
+        ...state,
+        ...action.payload,
+      ];
+    case ADD_APP:
+      return [
+        ...state,
+        action.payload,
+      ]
+
+    default:
+      return state;
+  }
+
 };
+
+export const fetchApp = (appointments) => ({
+  type: FETCH_APP,
+  payload: appointments,
+});
+
+export const fetchAppThunk = () => (dispatch) => fetch('appointmentsURL')
+  .then((response) => response.json())
+  .then((data) => {
+    dispatch(fetchApp(data));
+  });
+
+export const addApp = (appointment) => ({
+  type: ADD_APP,
+  payload: appointment,
+});
+
+export const addAppThunk = (appointment) => (dispatch) => fetch('appointmentsURL',
+  {
+    method: 'POST',
+    body: JSON.stringify({
+      appointment
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }
+)
+.then(() => {
+  dispatch(addApp(appointment));
+});
 
 export default appointmentReducer;
--- src/redux/doctors/doctor.js
+++ src/redux/doctors/doctor.js
@@ -1,5 +1,54 @@
+const ADD_DOC = 'doctorApp/doctors/ADD_DOCTOR';
+const FETCH_DOC = 'doctorApp/doctors/FETCH_DOC';
+
 const doctorReducer = (state = intial, action) => {
-  return state;
+  switch (action.type) {
+    case FETCH_DOC:
+      return [
+        ...state,
+        ...action.payload,
+      ];
+    case ADD_DOC:
+      return [
+        ...state,
+        action.payload,
+      ]
+
+    default:
+      return state;
+  }
+
 };
+
+export const fetchDoc = (doctors) => ({
+  type: FETCH_DOC,
+  payload: doctors,
+});
+
+export const fetchDocThunk = () => (dispatch) => fetch('doctorsURL')
+  .then((response) => response.json())
+  .then((data) => {
+    dispatch(fetchDoc(data));
+  });
+
+export const addDoc = (doctor) => ({
+  type: ADD_DOC,
+  payload: doctor,
+});
+
+export const addDocThunk = (doctor) => (dispatch) => fetch('doctorsURL',
+  {
+    method: 'POST',
+    body: JSON.stringify({
+      doctor
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }
+)
+.then(() => {
+  dispatch(addDoc(doctor));
+});
 
 export default doctorReducer;
