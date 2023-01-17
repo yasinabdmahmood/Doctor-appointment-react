37976965b8f4dbacbb3875b5c2131e2bb99c461a
yasinabdmahmood <abd181103@gmail.com>
2023-01-17 14:05:03 +0300

    Implment SignUp functionality

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,11 +1,79 @@
-import React from 'react';
+import React, { useState } from "react";
+import axios from "axios";
 
-function SignUp() {
+const SignUp = () => {
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const data = { name, username, email, password };
+
+    axios
+      .post("http://127.0.0.1:3000/users", data)
+      .then((res) => {
+        const loginData = { email, password };
+        axios
+          .post("http://127.0.0.1:3000/auth/login", loginData)
+          .then((res) => {
+            sessionStorage.setItem("token", res.data.token);
+            sessionStorage.setItem("isAdmin", res.data.isAdmin);
+            console.log(res.data.token)
+            console.log(res.data.isAdmin)
+          })
+          .catch((err) => {
+            console.log("Error making second API call: ", err);
+          });
+      })
+      .catch((err) => {
+        console.log("Error making API call: ", err);
+      });
+  };
+
   return (
-    <div>
-      <h1>SignUp form</h1>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name:
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </label>
+      <br />
+      <label>
+        Username:
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </label>
+      <br />
+      <label>
+        Email:
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      <br />
+      <button type="submit">Sign Up</button>
+    </form>
   );
-}
+};
 
 export default SignUp;
