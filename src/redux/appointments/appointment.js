0cc64d9ea7aa5ad9b1203d7c0650b4611e33465a
Kamba56 <tomajohn316@yahoo.com>
2023-01-17 20:51:05 +0100

    Refactor appointment redux to use axios

--- src/redux/appointments/appointment.js
+++ src/redux/appointments/appointment.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 const ADD_APP = 'doctorApp/appointments/ADD_APPOINTMENT';
 const FETCH_APP = 'doctorApp/appointments/FETCH_APPOINTMENT';
 const initial = [];
@@ -25,29 +27,31 @@ export const fetchApp = (appointments) => ({
   payload: appointments,
 });
 
-export const fetchAppThunk = () => (dispatch) => fetch('appointmentsURL')
-  .then((response) => response.json())
-  .then((data) => {
-    dispatch(fetchApp(data));
+export const fetchAppThunk = () => (dispatch) => {
+  axios.defaults.headers.common['Authorization'] = `Bearer ${"eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoyLCJleHAiOjE2NzQ1MDkwODB9.X37qH49KPfjQKqlH745Ezw-sycLKUabDymrvaY-zxdM"}`;
+  axios.get('http://127.0.0.1:3000/reservations')
+    .then((response) => {
+      fetchApp(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
   });
+};
 
 export const addApp = (appointment) => ({
   type: ADD_APP,
   payload: appointment,
 });
 
-export const addAppThunk = (appointment) => (dispatch) => fetch('appointmentsURL',
-  {
-    method: 'POST',
-    body: JSON.stringify({
-      appointment,
-    }),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
-  .then(() => {
-    dispatch(addApp(appointment));
+export const addAppThunk = (appointment) => (dispatch) => {
+  axios.defaults.headers.common['Authorization'] = `Bearer ${"eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoyLCJleHAiOjE2NzQ1MDkwODB9.X37qH49KPfjQKqlH745Ezw-sycLKUabDymrvaY-zxdM"}`;
+  axios.post('http://127.0.0.1:3000/reservations',{date: '2022-5-1', city: 'test_city',doctor_id: 2})
+    .then((response) => {
+      addApp(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
   });
+};
 
 export default appointmentReducer;
