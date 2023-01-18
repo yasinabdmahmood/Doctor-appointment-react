15f00979393175ca83376a94246a72c612860182
Kamba56 <tomajohn316@yahoo.com>
2023-01-18 21:39:17 +0100

    Add api calls to components

--- src/components/DoctorsList/DoctorsList.js
+++ src/components/DoctorsList/DoctorsList.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchDocThunk } from '../../redux/doctors/doctor';
 
-function DoctorsList() {
+const DoctorsList = () => {
+  const doctors = useSelector((store) => store.doctors);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchDocThunk());
+  }, [dispatch]);
+
   return (
     <div>
       <h1>Doctors list</h1>
+      {
+        doctors.map((doc) => (
+          <p key={doc.id}>{doc.name}</p>
+        ))
+      }
     </div>
   );
-}
+};
 
 export default DoctorsList;
--- src/components/MyReservations/MyReservations.js
+++ src/components/MyReservations/MyReservations.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchAppThunk } from '../../redux/appointments/appointment';
 
-function MyReservations() {
+const MyReservations = () => {
+  const appointments = useSelector((store) => store.appointments);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchAppThunk());
+  }, [dispatch]);
+
   return (
     <div>
       <h1>My reservations</h1>
+
+      {appointments.map((appointment) => (
+        <p key={appointment.id}>{appointment.city}</p>
+      ))}
     </div>
   );
-}
+};
 
 export default MyReservations;
