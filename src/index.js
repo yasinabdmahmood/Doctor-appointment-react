96318232a5e24775a8ea9b4859166586f5d9ad96
yasinabdmahmood <abd181103@gmail.com>
2023-01-18 12:57:15 +0300

    Add Provider component to the app

--- src/index.js
+++ src/index.js
@@ -6,12 +6,16 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import store  from './redux/configureStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <Provider store={store} >
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
+  </BrowserRouter>
+  </Provider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
