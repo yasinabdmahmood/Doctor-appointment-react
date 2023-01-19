8eb08b1e6d197a22b305eafbe88293e1227d212b
Peter Njuguna <Ptahbn@gmail.com>
2023-01-19 11:10:50 +0300

    component that allows the user to add new doctors.

--- src/components/AddDoctor/AddDoctorForm.js
+++ src/components/AddDoctor/AddDoctorForm.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addDoc } from '../../redux/doctors/doctor';
 
-function AddDoctorForm() {
+const AddDoctorForm = () => {
+  const [name, setName] = useState('');
+  const [specialization, setSpecialization] = useState('');
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(addDoc(name, specialization));
+  };
+
   return (
-    <div>
-      <h1>Add doctor form</h1>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name:
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+      </label>
+      <label>
+        Specialization:
+        <input
+          type="text"
+          value={specialization}
+          onChange={(e) => setSpecialization(e.target.value)}
+        />
+      </label>
+      <input type="submit" value="Add Doctor" />
+    </form>
   );
-}
+};
 
 export default AddDoctorForm;
