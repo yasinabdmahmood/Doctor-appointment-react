6b108857658fed4f22b2996b55ae08d7e402797e
Peter Njuguna <Ptahbn@gmail.com>
2023-01-19 11:10:25 +0300

    component that shows the details of a selected doctor.

--- src/components/DoctorDetails/DoctorDetails.js
+++ src/components/DoctorDetails/DoctorDetails.js
@@ -1,11 +1,29 @@
-import React from 'react';
+/* eslint-disable react/button-has-type */
+/* eslint-disable no-undef */
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
-function DoctorDetails() {
+const DoctorDetail = () => {
+  const { id } = useParams();
+  const [doctor, setDoctor] = useState({});
+  const doctors = useSelector((state) => state.doctors);
+
+  useEffect(() => {
+    setDoctor(doctors.find((doc) => doc.id === id));
+  }, [doctors, id]);
+
   return (
     <div>
-      <h1>Doctor details</h1>
+      <h1>{doctor.name}</h1>
+      <p>
+        Specialization:
+        {' '}
+        {doctor.specialization}
+      </p>
+      <button onClick={() => handleReserve()}>Reserve</button>
     </div>
   );
-}
+};
 
-export default DoctorDetails;
+export default DoctorDetail;
