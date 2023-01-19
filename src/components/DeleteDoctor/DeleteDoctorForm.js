2dd9e26cadc8d2eedffa2156c20ca31269ad3b41
Peter Njuguna <Ptahbn@gmail.com>
2023-01-19 11:11:06 +0300

    component that allows the user to delete doctors.

--- src/components/DeleteDoctor/DeleteDoctorForm.js
+++ src/components/DeleteDoctor/DeleteDoctorForm.js
@@ -1,11 +1,27 @@
+/* eslint-disable react/button-has-type */
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { removeDoc } from '../../redux/doctors/doctor';
 
-function DeleteDoctorForm() {
+const DeleteDoctorForm = () => {
+  const doctors = useSelector((state) => state.doctors);
+  const dispatch = useDispatch();
+
+  const handleDelete = (id) => {
+    dispatch(removeDoc(id));
+  };
+
   return (
     <div>
       <h1>Delete Doctor</h1>
+      {doctors.map((doctor) => (
+        <div key={doctor.id}>
+          <p>{doctor.name}</p>
+          <button onClick={() => handleDelete(doctor.id)}>Delete</button>
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default DeleteDoctorForm;
