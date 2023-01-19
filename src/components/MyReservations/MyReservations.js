66b896e32bb213363187f0d63f116135681ca475
Peter Njuguna <Ptahbn@gmail.com>
2023-01-19 11:11:23 +0300

    component that shows the user's reservations.

--- src/components/MyReservations/MyReservations.js
+++ src/components/MyReservations/MyReservations.js
@@ -1,11 +1,38 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-function MyReservations() {
+const MyReservations = () => {
+  const currentUser = useSelector((state) => state.currentUser);
+  const reservations = useSelector((state) => state.reservations);
+
+  const userReservations = reservations.filter(
+    (reservation) => reservation.username === currentUser.username,
+  );
+
   return (
     <div>
-      <h1>My reservations</h1>
+      <h1>My Reservations</h1>
+      {userReservations.map((reservation) => (
+        <div key={reservation.id}>
+          <p>
+            Item:
+            {' '}
+            {reservation.itemName}
+          </p>
+          <p>
+            Date:
+            {' '}
+            {reservation.date}
+          </p>
+          <p>
+            City:
+            {' '}
+            {reservation.city}
+          </p>
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default MyReservations;
