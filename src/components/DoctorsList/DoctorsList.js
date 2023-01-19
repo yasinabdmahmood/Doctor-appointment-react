d6b8ac09289b190ee176fd19da38acdeaf8d6429
Peter Njuguna <Ptahbn@gmail.com>
2023-01-19 11:09:58 +0300

    component that shows a list of doctors.

--- src/components/DoctorsList/DoctorsList.js
+++ src/components/DoctorsList/DoctorsList.js
@@ -1,11 +1,27 @@
-import React from 'react';
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/jsx-no-undef */
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchDoc } from '../../redux/doctors/doctor';
 
-function DoctorsList() {
+const DoctorList = () => {
+  const [doctors, setDoctors] = useState([]);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchDoc()).then((data) => setDoctors(data));
+  }, [dispatch]);
+
   return (
     <div>
-      <h1>Doctors list</h1>
+      <h1>Doctor List</h1>
+      {doctors.map((doctor) => (
+        <div key={doctor.id}>
+          <Link to={`/doctors/${doctor.id}`}>{doctor.name}</Link>
+        </div>
+      ))}
     </div>
   );
-}
+};
 
-export default DoctorsList;
+export default DoctorList;
