376de9eb2c4fd51e8c528c8f4cb68f97b8741146
benardop <benard.oc.p@gmail.com>
2023-01-23 11:24:11 +0300

    Add redux constants file

---
+++ src/redux/constants/Constants.js
@@ -0,0 +1,9 @@
+/* eslint-disable import/prefer-default-export */
+export const Booking = {
+  ALL_DOCTORS: 'ALL_DOCTORS',
+  GET_DOC_ID: 'GET_DOC_ID',
+  ONE_DOCTOR: 'ONE_DOCTOR',
+  ALL_RESERVATIONS: 'ALL_RESERVATIONS',
+  ONE_RESERVATION: 'ONE_RESERVATION',
+  CREATE_RESERVATION: 'CREATE_RESERVATION',
+};
