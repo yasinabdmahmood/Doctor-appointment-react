7cf07deea25a04187cb4e52a5a88254b860372ba
Peter Njuguna <Ptahbn@gmail.com>
2023-01-25 14:54:10 +0300

    Modify delete doctor form

--- src/components/DeleteDoctor/DeleteDoctorForm.js
+++ src/components/DeleteDoctor/DeleteDoctorForm.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { removeDocThunk } from '../../redux/doctors/doctor';
+import './DeleteDoctorForm.css';
 
-function DeleteDoctorForm() {
+const DeleteDoctorForm = () => {
+  const doctors = useSelector((state) => state.doctors);
+  const dispatch = useDispatch();
+
+  const handleDelete = (id) => {
+    dispatch(removeDocThunk(id));
+  };
+
   return (
-    <div>
+    <div className="delete-doctor-form">
       <h1>Delete Doctor</h1>
+      <ul>
+        {doctors.map((doctor) => (
+          <li key={doctor.id}>
+            {doctor.name}
+            <button type="button" onClick={() => handleDelete(doctor.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default DeleteDoctorForm;
