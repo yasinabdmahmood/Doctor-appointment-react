a45de3db414d5a6212296dd3649d808098db68f9
yasinabdmahmood <abd181103@gmail.com>
2023-01-26 12:54:29 +0300

    Fix bug in displaying reservations list

--- src/components/MyReservations/MyReservations.js
+++ src/components/MyReservations/MyReservations.js
@@ -2,13 +2,29 @@ import React, { useEffect, useState } from 'react';
 // import { useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchAppThunk } from '../../redux/appointments/appointment';
+import { fetchDocThunk } from '../../redux/doctors/doctor';
 
 const MyReservations = () => {
-  const reservationdatas = useSelector((state) => state.appointments);
-  const doctorsList = useSelector((state) => state.doctors);
+  const getDoctorNames = (state) => {
+    const doctors = state.doctors;
+    const reservations = state.appointments;
+    const appointments = reservations.map((reservation)=>{
+      return {
+        ...reservation,
+        name: doctors.filter((doctor)=> reservation.doctor_id === doctor.id )[0].name,
+      }
+      
+    });
+    console.log(appointments)
+    return appointments
+  }
+  const reservationdatas = useSelector((state) => getDoctorNames(state));
+  
+  //const doctorsList = useSelector((state) => state.doctors);
   const dispatch = useDispatch();
   // const { id } = useParams(1);
   useEffect(() => {
+    dispatch(fetchDocThunk());
     dispatch(fetchAppThunk());
   }, []);
 
@@ -18,9 +34,10 @@ const MyReservations = () => {
       <ul>
         {
           reservationdatas.map((item) => {
-            const { doctor_id, city, date } = item;
-            const doctor = doctorsList.filter((doctor) => doctor.id === parseInt(id, 10));
-            const name = doctor.name;
+            const { doctor_id, city, date, name } = item;
+            // const doctor = doctorsList.filter((doctor) => doctor.id === doctor_id);
+            // console.log(doctor[0].name)
+            // const name = doctor[0].name;
             return (
               <div key={doctor_id}>
                 <h5>reservations</h5>
