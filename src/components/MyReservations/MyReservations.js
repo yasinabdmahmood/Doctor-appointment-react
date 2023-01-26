f4560819f9e086182152c0fdfafbcf0544bf0b73
benardop <benard.oc.p@gmail.com>
2023-01-26 10:50:22 +0300

    Add a list of all reservations

--- src/components/MyReservations/MyReservations.js
+++ src/components/MyReservations/MyReservations.js
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchAppThunk } from '../../redux/appointments/appointment';
 
-function MyReservations() {
+const MyReservations = () => {
+  const dispatch = useDispatch();
+  const reservationdatas = useSelector((state) => state.reservationReducer);
+  const userID = 1;
+  const { reservations } = reservationdatas;
+
+  useEffect(() => {
+    dispatch(fetchAppThunk(userID));
+  }, []);
+
   return (
-    <div>
-      <h1>My reservations</h1>
-    </div>
+    <section>
+      <h2>My Reservations</h2>
+      <ul>
+        {
+          reservations[0].map((item) => {
+            const { name, city, date } = item.doctor;
+            const { id } = item.reservation;
+            return (
+              <div key={id}>
+                <h5>reservation</h5>
+                <p>
+                  reservation date:
+                  {' '}
+                  {date}
+                </p>
+                <p>
+                  doctor name:
+                  {' '}
+                  {name}
+                </p>
+                <p>
+                  doctor city:
+                  {' '}
+                  {city}
+                </p>
+              </div>
+            );
+          })
+        }
+      </ul>
+    </section>
   );
-}
+};
 
 export default MyReservations;
