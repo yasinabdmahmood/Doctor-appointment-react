9b154cdbc83350415cc0c74a75e0c55c63da2018
benardop <benard.oc.p@gmail.com>
2023-01-26 18:22:23 +0300

    Add reservation details

--- src/components/MyReservations/MyReservations.js
+++ src/components/MyReservations/MyReservations.js
@@ -1,40 +1,38 @@
-import React, { useEffect, useState } from 'react';
-// import { useParams } from 'react-router-dom';
+/* eslint-disable camelcase */
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchAppThunk } from '../../redux/appointments/appointment';
 import { fetchDocThunk } from '../../redux/doctors/doctor';
+import './MyReservations.css';
 
 const MyReservations = () => {
   const getDoctorNames = (state) => {
-    const doctors = state.doctors;
+    const { doctors } = state;
     const reservations = state.appointments;
-    const appointments = reservations.map((reservation)=>{
-      return {
-        ...reservation,
-        name: doctors.filter((doctor)=> reservation.doctor_id === doctor.id )[0].name,
-      }
-      
-    });
-    console.log(appointments)
-    return appointments
-  }
+    const appointments = reservations.map((reservation) => ({
+      ...reservation,
+      name: doctors.filter((doctor) => reservation.doctor_id === doctor.id)[0].name,
+    }));
+    console.log(appointments);
+    return appointments;
+  };
   const reservationdatas = useSelector((state) => getDoctorNames(state));
-  
-  //const doctorsList = useSelector((state) => state.doctors);
+
   const dispatch = useDispatch();
-  // const { id } = useParams(1);
   useEffect(() => {
     dispatch(fetchDocThunk());
     dispatch(fetchAppThunk());
   }, []);
 
   return (
-    <section>
+    <section className="reservation-wrapper">
       <h2>My Reservations</h2>
       <ul>
         {
           reservationdatas.map((item) => {
-            const { doctor_id, city, date, name } = item;
+            const {
+              doctor_id, city, date, name,
+            } = item;
             // const doctor = doctorsList.filter((doctor) => doctor.id === doctor_id);
             // console.log(doctor[0].name)
             // const name = doctor[0].name;
